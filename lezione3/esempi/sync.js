d5dbed6403b5393178c6f5070c4eb58b9bd7f734
Roberto Serra <roberto.serra@etiqa.it>
2022-04-13 08:50:06 +0200

    lezione 3 - esempi e scripts

---
+++ lezione3/esempi/sync.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+const utils = require("../utils");
+
+const starting = utils.getNow();
+
+// Questa funzione genera un wait sincrono
+function syncWait(ms) {
+    console.log(`Sync operation starting after: ${utils.getNow() - starting}ms`);
+    const now = utils.getNow();
+  while (utils.getNow() <= now + ms) {}
+  console.log(`Sync operation finished after: ${utils.getNow() - starting}ms`);
+
+}
+
+console.log(`Starting`);
+
+syncWait(3000);
+console.log(`Next operation starting after: ${utils.getNow() - starting}ms`);
+syncWait(1000);
+console.log(`Finished in ${utils.getNow() - starting}ms`);
